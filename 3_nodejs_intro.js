ab10e5f9af50afd92bca530ad829ab59d8915e0b
kongfanteng <18210591009@163.com>
2023-08-01 10:59:45 +0800

    feat: 什么是 Node.js

---
+++ 3_nodejs_intro.js
@@ -0,0 +1,30 @@
+/**
+ * @example
+ *
+ * ```js
+    // 什么是 Node.js
+    // - Node.js is a JavaScript runtime built on Chrome's V5 JavaScript engine.
+    // - Node.js 是一个基于 Chrome V8 引擎的 JavaScript 运行环境
+    // - Node.js 的官网地址 {@link https://nodejs.org/zh-cn}
+
+    // Node.js 的 JavaScript 运行环境
+    // - 浏览器是 JavaScript 的前端运行环境
+    // - Node.js 是 JavaScript 的后端运行环境
+    // - Node.js 中无法调用 DOM 和 BOM 等浏览器内置 API
+    // - Node.js 运行环境：内置 API（fs、path、http、JS 内置对象、querystring...）-> 待执行的 JavaScript 代码 -> V8 引擎
+
+    // Node.js 可以做什么
+    // - Node.js 作为一个 JavaScript 的运行环境，仅仅提供了基础的功能和 API。然而，基于 Node.js 提供的这些基本功能，很多强大的工具和框架如雨后春笋，层出不穷，所以学会了 Node.js，可以让前端程序员胜任更多的工作和岗位：
+    // - - 基于 Express 框架 {@link http://www.expressjs.com.cn}，可以快速构建 Web 应用
+    // - - 基于 Electron 框架 {@link http://electron.org/}，可以构建跨平台的桌面应用
+    // - - 基于 resify 框架 {@link http://restify.com/}，可以快速构建 API 接口项目
+    // - - 读写和操作数据库、创建实用的命令行工具辅助前端开发、etc...
+
+    // Node.js 怎么学
+    // - 浏览器中的 JavaScript 学习路径
+    // - - JavaScript 基础语法 + 浏览器内置 API（DOM + BOM）+ 第三方库（jQuery、art-template 等）
+    // - Node.js 的学习路径
+    // - - JavaScript 基础语法 + Node.js 内置 API 模块（fs、path、http 等）+ 第三方 API 模块（express、mysql 等）
+ * ```
+ * 
+ */
