deac68bfc64c08307444a7fab1bf23e5cb5dc7ab
kongfanteng <18210591009@163.com>
2023-08-01 11:09:14 +0800

    feat: 安装 Node.js

---
+++ 4_nodejs_install.js
@@ -0,0 +1,18 @@
+/**
+ * @example
+ *
+ * ```js
+    // 安装 Node.js
+    // - Node.js 环境的安装
+    // - - 如果希望通过 Node.js 来运行 JavaScript 代码，则必须在计算机上安装 Node.js 环境
+    // - - 安装包可以从 Node.js 的官网首页直接下载，进入到 Node.js 的官网首页 {@link https://nodejs.org/en/}，点击绿色的按钮，下载所需的版本后，双击直接安装即可。
+    // - 区分 LTS 版本和 Current 版本的不同
+    // - - LTS 为长期稳定版，对于追求稳定性的企业级项目来说，推荐安装 LTS 版本的 Node.js
+    // - - Current 为新特性尝鲜版，对热衷于尝试新特性的用户来说，推荐安装 Current 版本的 Node.js。但是，Current 版本中可能存在隐藏的 Bug 或安全性漏洞，因此不推荐在企业级项目中使用 Current 版本的 Node.js。
+    // - 查看已安装的 Node.js 的版本号
+    // - - 打开终端，在终端输入命令 node -v 后，按下回车键，即可查看已安装的 Node.js 的版本号
+    // - - Windows 系统快速打开终端的方式
+    // - - - 使用快捷键（Window 徽标键 + R）打开运行面板，输入 cmd 后直接回车，即可打开终端。
+ * ```
+ * 
+ */
