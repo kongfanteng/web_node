cec22a1656dbc4e056660ef9e10ad55dd6bd11f9
kongfanteng <18210591009@163.com>
2023-08-03 20:14:02 +0800

    fix: module-url 添加 file://

--- 20_module_dateFormat.js
+++ 20_module_dateFormat.js
@@ -4,11 +4,11 @@
  * ```js
     // 包与 npm：格式化时间的两种做法
     // - 1、格式化时间的传统做法？6 个步骤
-    // - - 1）创建格式化时间的自定义模块：./file/module/dateFormat.js
+    // - - 1）创建格式化时间的自定义模块：{@link file://./file/module/dateFormat.js}
     // - - 2）定义格式化时间的方法：dateFormat 
     // - - 3）创建补零函数：padZero
     // - - 4）从自定义模块中导出格式化时间的函数
-    // - - 5）导入格式化时间的自定义模块：./file/module/12.格式化时间的传统做法.js
+    // - - 5）导入格式化时间的自定义模块：{@link file://./file/module/12.格式化时间的传统做法.js}
     // - - 6）调用格式化时间的函数：Time.dateFormat(dt)
     // - 2、格式化时间的高级做法
     // - - 1）使用 npm 包管理工具，在项目中安装格式化时间的包 moment；运行命令 npm install moment;
@@ -16,7 +16,12 @@
     // - - 3）参考 moment 的官方 API 文档对时间进行格式化
     // - - - 调用 moment() 方法，得到当前的时间
     // - - - 针对当前的时间，调用 format() 方法，按照指定的格式进行时间的格式化
-    // - - 代码: {@link ./file/module/20.moment.js}
+    // - - 代码: {@link file://./file/module/20.moment.js}
+    // - 3、JS 导入 moment 包 npmjs 官网搜索 moment，点击 Document，查看 moment 文档 
+    // - - npmjs 官网 {@link http://npmjs.com}
+    // - - moment 文档 {@link https://momentjs.com/docs/}
+    // - - 注意：首次使用包，通过查询官方文档使用包
+   
  * ```
  * 
  */
