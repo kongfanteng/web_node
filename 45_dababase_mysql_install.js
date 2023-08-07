245621f5c7e0225988d7b19810e2c3385ee61c19
kongfanteng <18210591009@163.com>
2023-08-07 10:34:15 +0800

    feat: Database-安装 MySQL 数据库

---
+++ 45_dababase_mysql_install.js
@@ -0,0 +1,21 @@
+/**
+ * @example
+ *
+ * ```js
+    // Database-安装 MySQL 数据库
+    // - 1、了解安装哪些 MySQL 相关的软件
+    // - - 对于开发人员来说，只需要安装 MySQL Server 和 MySQL Workbench 这两个软件，就能满足开发的需要了。
+    // - - - MySQL Server：专门用来提供数据存储和服务的软件
+    // - - - MySQL Workbench：可视化 MySQL 管理工具，通过它，可以方便的操作存储在 MySQL Server 中的数据
+
+    // - 2、MySQL 在 Mac 环境下安装
+    // - - 在 Mac 环境下安装 MySQL 的过程比 Windows 环境下的步骤简单很多：
+    // - - - 1）先运行 mysql-8.0.19-macos10.15-x86_64.dmg 这个安装包，将 MySQL Server 安装到 Mac 系统 {@link https://dev.mysql.com/downloads/mysql/}
+    // - - - 2）再运行 mysql-workbench-community-8.0.19-macos-x86_64.dmg 这个安装包，将可视化的 MySQL Workbench 工具安装到 Mac 系统 {@link https://dev.mysql.com/downloads/workbench/}
+
+    // - 3、MySQL 在 Windows 环境下安装
+    // - - 在 Windows 环境安装 MySQL，需需要运行 mysql-install-community-8.0.19.0.msi 这个安装包，就能一次性将 MySQL Server 和 MySQL Workbench 安装到自己的电脑上
+    
+ * ```
+ *
+ */
