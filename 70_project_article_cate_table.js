6c078ff3182a6679cd3c5350bd1a117946cb1d26
kongfanteng <18210591009@163.com>
2023-08-10 09:46:30 +0800

    feat: Project-Article-新建 ev_article_cate 表

---
+++ 70_project_article_cate_table.js
@@ -0,0 +1,15 @@
+/**
+ * @example
+ *
+ * ```js
+    // Project-Article-新建 ev_article_cate 表
+    // - 1、创建表结构
+    // - - Table Name: ev_article_cate; Comments: 文章分类表; id-INT-PK-NN-UQ-AI; name-VARCHAR(255)-NN-UQ; alias-VARCHAR(255)-NN-UQ; is_delete-TINYINT(1)-NN-Default(0)-Comments, 数据是否被标记删除 0 没有删除 1 被删除;
+
+    // - 1、新增两条初始数据
+    // - - name-科技; alias-Science and Technology;
+    // - - name-历史; alias-History;
+
+ * ```
+ *
+ */
